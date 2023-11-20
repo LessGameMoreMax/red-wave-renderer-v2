49488a6f0ccb6aa23fa5ac58f6a45d6a09cf7fab
LessGameMoreMax <LessGameMoreMax@outlook.com>
2023-11-20 21:37:20 +0800

    23/11/20

---
+++ Source/Runtime/Core/Meta/ReflStatic.h
@@ -0,0 +1,192 @@
+#ifndef REFL_STATIC_H
+#define REFL_STATIC_H
+#include "../Misc/MacroTools.h"
+#include <tuple>
+#include <type_traits>
+namespace sablin{
+
+#define _FIELD_CONSTRUCT(FIELD, ATTR) lenin::_Field{lenin::_TypeContainer<decay_t<decltype(ClassType::FIELD)>>{}, MACRO_TO_STRING(FIELD), &ClassType::FIELD, ATTR} 
+
+#define _SPEARD_FIELD_2(FIELD, ATTR)       _FIELD_CONSTRUCT(FIELD, ATTR)
+#define _SPEARD_FIELD_4(FIELD, ATTR, ...)  _FIELD_CONSTRUCT(FIELD, ATTR), _SPEARD_FIELD_2(__VA_ARGS__)
+#define _SPEARD_FIELD_6(FIELD, ATTR, ...)  _FIELD_CONSTRUCT(FIELD, ATTR), _SPEARD_FIELD_4(__VA_ARGS__)
+#define _SPEARD_FIELD_8(FIELD, ATTR, ...)  _FIELD_CONSTRUCT(FIELD, ATTR), _SPEARD_FIELD_6(__VA_ARGS__)
+#define _SPEARD_FIELD_10(FIELD, ATTR, ...) _FIELD_CONSTRUCT(FIELD, ATTR), _SPEARD_FIELD_8(__VA_ARGS__)
+#define _SPEARD_FIELD_12(FIELD, ATTR, ...) _FIELD_CONSTRUCT(FIELD, ATTR), _SPEARD_FIELD_10(__VA_ARGS__)
+#define _SPEARD_FIELD_14(FIELD, ATTR, ...) _FIELD_CONSTRUCT(FIELD, ATTR), _SPEARD_FIELD_12(__VA_ARGS__)
+#define _SPEARD_FIELD_16(FIELD, ATTR, ...) _FIELD_CONSTRUCT(FIELD, ATTR), _SPEARD_FIELD_14(__VA_ARGS__)
+
+#define _SPEARD_CLASS_1(CLASS) \
+namespace sablin{                                                  \
+    template<>                                                     \
+    struct ClassInfo<CLASS>: public lenin::_ClassInfoBase<CLASS>{  \
+        friend class HasParent1<CLASS>;                            \
+        friend class HasParent2<CLASS>;                            \
+        using ClassType = CLASS;                                   \
+    private:                                                       \
+        using ParentType1 = NoneType;                              \
+        using ParentType2 = NoneType;                              \
+    public:                                                        \
+
+#define _SPEARD_CLASS_2(CLASS, PARENT) \
+namespace sablin{                                                  \
+    template<>                                                     \
+    struct ClassInfo<CLASS>: public lenin::_ClassInfoBase<CLASS>{  \
+        friend class HasParent1<CLASS>;                            \
+        friend class HasParent2<CLASS>;                            \
+        using ClassType = CLASS;                                   \
+        using ParentType1 = PARENT;                                \
+    private:                                                       \
+        using ParentType2 = NoneType;                              \
+    public:                                                        \
+
+#define _SPEARD_CLASS_3(CLASS, PARENT1, PARENT2) \
+namespace sablin{                                                  \
+    template<>                                                     \
+    struct ClassInfo<CLASS>: public lenin::_ClassInfoBase<CLASS>{  \
+        friend class HasParent1<CLASS>;                            \
+        friend class HasParent2<CLASS>;                            \
+        using ClassType = CLASS;                                   \
+        using ParentType1 = PARENT1;                               \
+        using ParentType2 = PARENT2;                               \
+
+#define SR_CLASS(...) \
+    MACRO_LINK(_SPEARD_CLASS_, ARG_COUNT(__VA_ARGS__))(__VA_ARGS__) \
+
+#define SR_END() \
+    };   \
+}        \
+
+#define SR_FIELDS(...) \
+    inline static constexpr const lenin::_FieldsInfo _fields_info{                             \
+        MACRO_LINK(_SPEARD_FIELD_, ARG_COUNT(__VA_ARGS__))(__VA_ARGS__)};                      \
+    inline static constexpr const size_t get_fields_count(){                                   \
+        size_t size = std::tuple_size<decltype(_fields_info.fields_tuple_)>::value;            \
+        if constexpr(HasParent1<ClassType>::Value){                                            \
+            size += ClassInfo<ParentType1>::get_fields_count();                                \
+        }                                                                                      \
+        if constexpr(HasParent2<ClassType>::Value){                                            \
+            size += ClassInfo<ParentType2>::get_fields_count();                                \
+        }                                                                                      \
+        return size;                                                                           \
+    }                                                                                          \
+    template<int32_t index>                                                                    \
+    inline static constexpr const auto& get_field(){                                           \
+        constexpr int32_t size = std::tuple_size<decltype(_fields_info.fields_tuple_)>::value; \
+        if constexpr(index < size){                                                            \
+            return std::get<index>(_fields_info.fields_tuple_);                                \
+        }else{                                                                                 \
+            constexpr int32_t index1 = index - size;                                           \
+            constexpr int32_t size1 = ClassInfo<ParentType1>::get_fields_count();              \
+            if constexpr(index1 < size1){                                                      \
+                return ClassInfo<ParentType1>::get_field<index1>();                            \
+            }else{                                                                             \
+                return ClassInfo<ParentType2>::get_field<index1 - size1>();                    \
+            }                                                                                  \
+        }                                                                                      \
+    }                                                                                          \
+    inline static constexpr int32_t get_field_by_field_name(const std::string_view field_name){ \
+        size_t result_index = -1;                                                              \
+        auto f = [&](const size_t index, const std::string_view& name){                        \
+            if(field_name.compare(name) == 0) result_index = index;                            \
+        };                                                                                     \
+        [&]<size_t... I>(std::index_sequence<I...>){                                           \
+            ((f(I, get_field<I>().field_name_)), ...);                                         \
+        }(std::make_index_sequence<get_fields_count()>{});                                     \
+        return result_index;                                                                   \
+    }                                                                                          \
+
+namespace lenin{
+
+template<typename T>
+struct _ClassInfoBase{
+    inline static constexpr const std::string_view get_class_type_name(){
+        std::string_view type_name;
+#ifdef __clang__
+        type_name = __PRETTY_FUNCTION__;
+        auto head = type_name.find("T = ") + 4;
+        auto tail = type_name.find_last_of(';');
+#elif defined(__GNUC__)
+        type_name = __PRETTY_FUNCTION__;
+        auto head = type_name.find("T = ") + 4;
+        auto tail = type_name.find_last_of(';');
+#elif defined(_MSC_VER)
+        type_name = __FUNCSIG__;
+        auto head = type_name.find("ClassInfoBase<") + 11;
+        auto tail = type_name.find_last_of('>');
+#endif
+        return std::string_view{type_name.data() + head, tail - head};
+    }
+};
+
+template<typename T>
+struct _TypeContainer{ using Type = T; };
+
+template<typename P, typename M, typename... Args>
+struct _Field{
+    using MemberType = M;
+    std::string_view field_name_;
+    P pointer_;
+    std::tuple<Args...> attributes_;
+
+    explicit constexpr _Field(_TypeContainer<M>, const std::string_view field_name, P pointer, const std::tuple<Args...>& attributes):
+        field_name_(field_name),
+        pointer_(pointer),
+        attributes_(attributes){}
+
+    inline constexpr const std::string_view get_field_type_name() const{
+        std::string_view type_name;
+#ifdef __clang__
+        type_name = __PRETTY_FUNCTION__;
+        auto head = type_name.find("M = ") + 4;
+        auto tail = type_name.find_last_of(';');
+#elif defined(__GNUC__)
+        type_name = __PRETTY_FUNCTION__;
+        auto head = type_name.find("M = ") + 4;
+        auto tail = type_name.find_last_of(';');
+#endif
+        std::string_view temp{std::string_view{type_name.data() + head, tail - head}};
+        return std::string_view{temp.data(), temp.find_last_of(';')};
+    }
+};
+
+template<typename... Args>
+struct _FieldsInfo{
+    std::tuple<Args...> fields_tuple_;
+
+    explicit constexpr _FieldsInfo(Args... args):
+        fields_tuple_(std::forward<Args>(args)...){}
+};
+
+}
+
+template<typename T>
+struct ClassInfo: public lenin::_ClassInfoBase<T>{};
+
+struct NoneType{
+    int _none = 0;
+};
+
+template<>
+struct ClassInfo<NoneType>: public lenin::_ClassInfoBase<NoneType>{
+    inline static constexpr const size_t get_fields_count(){ return 0;}
+
+    template<int32_t index>
+    inline static constexpr const auto& get_field(){
+        return lenin::_Field{lenin::_TypeContainer<int>{}, "_none", &NoneType::_none, std::tuple{}};
+    }
+};
+
+template<typename T>
+struct HasParent1{
+    inline static constexpr const bool Value = !std::is_same_v<typename ClassInfo<T>::ParentType1, NoneType>;
+};
+
+template<typename T>
+struct HasParent2{
+    inline static constexpr const bool Value = !std::is_same_v<typename ClassInfo<T>::ParentType2, NoneType>;
+};
+
+
+}
+#endif
---
+++ Source/Runtime/Core/Meta/test/dev_test.cpp
@@ -0,0 +1,99 @@
+#include "../ReflStatic.h"
+#include <iostream>
+
+using namespace sablin;
+using namespace std;
+
+class Node{
+public:
+    int n_i = 1;
+    float n_f = 0.4;
+
+};
+
+class Node2{
+public:
+    int n_i2 = 12;
+    float n_f2 = 0.6;
+};
+
+class Child: public Node{
+public:
+    int c_i = 2;
+    float c_f = 0.2;
+
+};
+
+class Final: public Child, public Node2{
+public:
+    int f_i = 3;
+    float f_f = 0.5;
+};
+
+
+SR_CLASS(Node)
+    SR_FIELDS(n_i, std::tuple{1}, n_f, std::tuple{0.4})
+SR_END()
+
+SR_CLASS(Node2)
+    SR_FIELDS(n_i2, std::tuple{}, n_f2, std::tuple{})
+SR_END()
+
+SR_CLASS(Child, Node)
+    SR_FIELDS(c_i, std::tuple{2}, c_f, std::tuple{0.2})
+SR_END()
+
+SR_CLASS(Final, Child, Node2)
+    SR_FIELDS(f_i, std::tuple{}, f_f, std::tuple{})
+SR_END()
+
+int main(){
+    // cout << ClassInfo<Node>::get_class_type_name() << endl;
+    // cout << ClassInfo<Child>::get_class_type_name() << endl;
+    // cout << ClassInfo<ClassInfo<Child>::ParentType>::get_class_type_name() << endl;
+    // Node n;
+    // cout << std::get<0>(ClassInfo<Node>::_fields_info.fields_tuple_).field_name_ << endl;
+    // cout << std::get<1>(ClassInfo<Node>::_fields_info.fields_tuple_).field_name_ << endl;
+    // n.*std::get<0>(ClassInfo<Node>::_fields_info.fields_tuple_).pointer_ = 2;
+    // cout << n.a << endl;
+    // cout << std::get<0>(std::get<0>(ClassInfo<Node>::_fields_info.fields_tuple_).attributes_) << endl;
+    // cout << std::get<0>(ClassInfo<Child>::_fields_info.fields_tuple_).get_field_type_name() << endl;
+    // cout << std::get<1>(ClassInfo<Child>::_fields_info.fields_tuple_).get_field_type_name() << endl;
+    // cout << std::get<0>(ClassInfo<ClassInfo<Child>::ParentType>::_fields_info.fields_tuple_).get_field_type_name() << endl;
+    // cout << std::get<1>(ClassInfo<ClassInfo<Child>::ParentType>::_fields_info.fields_tuple_).get_field_type_name() << endl;
+    // cout << ClassInfo<Child>::get_field<0>().get_field_type_name() << endl;
+    // Node a;
+    // cout << ClassInfo<decltype(a)>::get_field<1>().get_field_type_name() << endl;
+    // cout << ClassInfo<Node>::get_fields_count() << endl;
+    // cout << ClassInfo<Node2>::get_fields_count() << endl;
+    // cout << ClassInfo<Child>::get_fields_count() << endl;
+    // cout << ClassInfo<Final>::get_fields_count() << endl;
+
+    // cout << ClassInfo<Child>::get_field<0>().field_name_ << endl;
+    // cout << ClassInfo<Child>::get_field<1>().field_name_ << endl;
+    // cout << ClassInfo<Child>::get_field<2>().field_name_ << endl;
+    // cout << ClassInfo<Child>::get_field<3>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<0>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<1>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<2>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<3>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<4>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<5>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<6>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field<7>().field_name_ << endl;
+    // cout << ClassInfo<Final>::get_field_by_field_name("x_f") << endl;
+    // cout << ClassInfo<Final>::get_field_by_field_name("ff") << endl;
+    // cout << ClassInfo<Final>::get_field_by_field_name("if") << endl;
+    //
+    // cout << ClassInfo<Final>::get_field_by_field_name("n_i") << endl;
+    // cout << ClassInfo<Final>::get_field_by_field_name("n_f") << endl;
+    //
+    // cout << ClassInfo<Final>::get_field_by_field_name("c_i") << endl;
+    // cout << ClassInfo<Final>::get_field_by_field_name("c_f") << endl;
+    //
+    // cout << ClassInfo<Final>::get_field_by_field_name("f_i") << endl;
+    // cout << ClassInfo<Final>::get_field_by_field_name("f_f") << endl;
+    //
+    // cout << ClassInfo<Final>::get_field_by_field_name("n_i2") << endl;
+    // cout << ClassInfo<Final>::get_field_by_field_name("n_f2") << endl;
+}
